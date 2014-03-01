8599f32dde75e53f3e89d55c76dde7bbc7726b41
Alexandru Nita <ncalexandru@gmail.com>
2014-03-01 02:01:05 +0200

    Added logic for up and down buttons

--- client/js/main.js
+++ client/js/main.js
@@ -8,8 +8,38 @@
 	      MOVE_BACKWARD = 'backward',
 	      STOP = 'stop';
 
+
+	   //TODO: unify handlers for actions
+
+	   //click handlers for buttons
+	  $('document').ready(function() {
+
+	      $('#controls button').on('mousedown press', function() {
+	          var keypress = $.Event('keypress');
+	          switch (this.id) {
+	              case MOVE_FORWARD:
+	                  // 119 = 'W'
+	                  keypress.keyCode = 119;
+	                  break;
+	              case MOVE_BACKWARD:
+	                  // 83 = 'S'
+	                  keypress.keyCode = 83;
+	                  break;
+	          }
+	          $('html').trigger(keypress);
+	      });
+
+	      $('#controls button').on('mouseup release', function() {
+	          var keyup = $.Event('keyup');
+	          // 83 = 'S'
+	          keyup.keyCode = 83;
+	          $('html').trigger(keyup);
+	      })
+	  })
+
+
 	   //handler for keypress on the html page
-	  $('html').keypress(function(e) {
+	   $('html').keypress(function(e) {
 	      var pressedKey = String.fromCharCode(e.keyCode).toUpperCase();
 	      switch (pressedKey) {
 	          case "W":
